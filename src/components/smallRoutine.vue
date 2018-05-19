e0744177b45cbbc0d0ac5abdadd9800e15ea6d17
xiexia.liu <xiexia.liu@future-data.cn>
2018-05-19 12:13:52 +0800

    x-circle优化

--- src/components/smallRoutine.vue
+++ src/components/smallRoutine.vue
@@ -2,20 +2,19 @@
   <div class="wx-body">
     <div class="text-center p1">附近小程序</div>
     <div class="text-center p1">
-      <div style='width:80%;height:20%;'>
+      <span class="inline-block" style='width:50%;height:20%;'>
         <x-circle :percent="percent1" :stroke-width="1" stroke-color="#04BE02">
           <span>{{percent1}}</span>
         </x-circle>
-      </div>
-      <br>
-      <div style="width:80%;">
-        <range v-model="percent1" :min="0" :max="100"></range>
-      </div>
-      <br>
-      <div style='width:80%;height:20%;'>
+      </span>
+      <span class="inline-block" style='width:50%;height:20%;'>
         <x-circle :percent="percent2" :stroke-width="6" :trail-width="6" :stroke-color="['#36D1DC', '#5B86E5']" trail-color="#ececec">
           <span :style="{color: strokeColor2}">{{percent2}}%</span>
         </x-circle>
+      </span>
+      <br>
+      <div style="width:80%;">
+        <range v-model="percent1" :min="0" :max="100"></range>
       </div>
     </div>
   </div>
